07243a65dfe8e7ccecf95485570d6fa2857c6cea
Erica <ericarhollis@gmail.com>
2015-07-19 20:56:19 -0600

    new commit

--- public/javascript/paperbird.js
+++ public/javascript/paperbird.js
@@ -8,15 +8,25 @@ $('#nav').affix({
     }
 });
 
+var public_images_portfolio_pTrump = ["Saab", "Volvo", "BMW"];
+
 function enlargify(link) {
 	$('.thumb').hide();
-	$('#large-image').attr("src", link);
+	$('#thumbnail').show();
+	//$('#large-image').attr("src", link);
 	$('#links').css("height", "400");
 	$('#large-image').css("height", "380");
+	$('#large-image').css("background-image",'url(' + link + ')');
 	$('#large-image-container').show();
 }
 
+//for each element that is classed as 'pull-down', set its margin-top to the difference between its own height and the height of its parent
+$('.bottom-aligned-text').each(function() {
+	$(this).css('margin-top', $(this).parent().parent().height()-$('#paperbird-main-logo').height()-$(this).height())
+});
+
 function goBack() {
 	$('.thumb').show();
+	$('#thumbnail').hide();
 	$('#large-image-container').hide();
 }
